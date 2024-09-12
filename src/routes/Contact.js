1fffe5827c9049c90e29e16513922310ad6a2a16
imanishita <imanishita17@gmail.com>
2024-09-13 02:19:54 +0530

    firstchange

---
+++ src/routes/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Information</h1>
+      <p>You can contact me through this page.</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/routes/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Project() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <p>Here are some of the projects I have worked on.</p>
+    </div>
+  );
+}
+
+export default Project;
