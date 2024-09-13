c49e92d0a1a351c81006ad0516cbc1d42b98c75a
imanishita <imanishita17@gmail.com>
2024-09-14 01:59:57 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import "./NavbarStyles.css";
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from "react-router-dom";
+import{FaBars,FaTimes} from "react-icons/fa";
+//import { useState } from "react";
+ 
 function Navbar() {
+    const [click,setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
       <div className="header">
         <Link to="/">
            <h1>Manishita Biswas</h1>
         </Link>
-        <ul className="nav-menu">
+        <ul className={click?"nav-menu active":"nav-menu"}>
             <li>
                 <Link to ="/">Home</Link>
             </li>
@@ -21,9 +26,18 @@ function Navbar() {
                 <Link to ="/contact">Contact</Link>
             </li>
         </ul>
+        <div className = "hamburger" onClick = {handleClick}>
+        {click ? (
+                    <FaTimes size={20} style={{ color: "#fff" }} />
+                ) : (
+                    <FaBars size={20} style={{ color: "#fff" }} />
+                )}
+        
+           
+        </div>
       </div>
-    )
-  }
+    );
+  };
   
   export default Navbar;
   
